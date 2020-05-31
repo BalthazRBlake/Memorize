76ff9c43ee89f186ad3add933a35f10ec0a3af98
FHHF <fabianhumbertohernandez@gmail.com>
2020-05-31 18:52:42 +0300

    property isFaceUp for CardView controls show back/front

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             ForEach(0..<4) { index in
-                CardView()
+                CardView(isFaceUp: false)
             }
         }
         .padding()
@@ -21,11 +21,17 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 10.0).fill(Color.gray)
-            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-            Text("👻").font(Font.largeTitle)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.gray)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0)
+            }
         }
     }
 }
